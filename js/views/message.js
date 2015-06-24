e7bfaa9eda8139ecd59778d9816a266bf510de19
Jeremy Scheff <jdscheff@gmail.com>
2015-06-23 21:43:44 -0400

    Get rid of flickering, but this seems wrong

--- js/views/message.js
+++ js/views/message.js
@@ -5,61 +5,59 @@
 define(["dao", "globals", "ui", "core/league", "lib/knockout", "lib/react", "util/viewHelpers"], function (dao, g, ui, league, ko, React, viewHelpers) {
     "use strict";
 
+    function updateMessage(mid) {
+//        if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || vm.message.mid() !== inputs.mid) {
+        var message, readThisPageview, tx;
+
+        tx = dao.tx("messages", "readwrite");
+
+        readThisPageview = false;
+
+        // If mid is null, this will open the *unread* message with the highest mid
+        dao.messages.iterate({
+            ot: tx,
+            key: mid,
+            direction: "prev",
+            callback: function (messageLocal, shortCircuit) {
+                message = messageLocal;
+
+                if (!message.read) {
+                    shortCircuit(); // Keep looking until we find an unread one!
+
+                    message.read = true;
+                    readThisPageview = true;
+
+                    return message;
+                }
+            }
+        });
+
+        return tx.complete().then(function () {
+            league.updateLastDbChange();
+
+            if (readThisPageview) {
+                if (g.gameOver) {
+                    ui.updateStatus("You're fired!");
+                }
+
+                return ui.updatePlayMenu(null);
+            }
+        }).then(function () {
+            return {
+                message: message
+            };
+        });
+//        }
+    }
+
     var CommentBox = React.createClass({
-        loadMessage: function () {
-            var message, readThisPageview, tx;
-
-            tx = dao.tx("messages", "readwrite");
-
-            readThisPageview = false;
-
-            // If mid is null, this will open the *unread* message with the highest mid
-            dao.messages.iterate({
-                ot: tx,
-                key: this.props.mid,
-                direction: "prev",
-                callback: function (messageLocal, shortCircuit) {
-                    message = messageLocal;
-
-                    if (!message.read) {
-                        shortCircuit(); // Keep looking until we find an unread one!
-
-                        message.read = true;
-                        readThisPageview = true;
-
-                        return message;
-                    }
-                }
-            });
-
-            tx.complete().then(function () {
-                league.updateLastDbChange();
-
-                if (readThisPageview) {
-                    if (g.gameOver) {
-                        ui.updateStatus("You're fired!");
-                    }
-
-                    return ui.updatePlayMenu(null);
-                }
-            }).then(function () {
-                if (this.isMounted()) {
-                    this.setState({message: message});
-                }
-            }.bind(this));
-        },
-        componentDidMount: function() {
-            this.loadMessage();
-        },
         render: function() {
-            if (this.state === null) { return <div>PLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDERPLACEHOLDER</div>; }
-
             return (
                 <div>
                     <h4 style={{marginTop: "23px"}}>
-                        From: <span>{this.state.message.from}</span>, <span>{this.state.message.year}</span> NW
+                        From: <span>{this.props.message.from}</span>, <span>{this.props.message.year}</span> NW
                     </h4>
-                    <span dangerouslySetInnerHTML={{__html: this.state.message.text}}></span>
+                    <span dangerouslySetInnerHTML={{__html: this.props.message.text}}></span>
                     <p><a href="INBOX LINK">Return To Inbox</a></p>
                 </div>
             );
@@ -72,17 +70,13 @@ define(["dao", "globals", "ui", "core/league", "lib/knockout", "lib/react", "uti
 
             mid = req.params.mid ? parseInt(req.params.mid, 10) : null;
 
+            return updateMessage(mid);
+        }).then(function (data) {
             React.render(
-              <CommentBox mid={mid} />,
-              document.getElementById('league_content')
+                <CommentBox message={data.message} />,
+                document.getElementById('league_content')
             );
         });
-    }
-
-    function updateMessage(inputs, updateEvents, vm) {
-        if (updateEvents.indexOf("dbChange") >= 0 || updateEvents.indexOf("firstRun") >= 0 || vm.message.mid() !== inputs.mid) {
-            
-        }
     }
 
     function uiFirst(vm) {
