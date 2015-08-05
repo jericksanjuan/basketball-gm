58bfe9182ad923a00603b0b51b76b7ffd422f3c8
Jerick Don San Juan <jerick@icannhas.com>
2015-08-05 09:44:09 +0800

    some error handling

--- js/core/player.js
+++ js/core/player.js
@@ -1654,6 +1654,7 @@ define(["dao", "globals", "core/finances", "data/injuries", "data/names", "lib/b
         mp = {};
         mp.value = cpuValue(p, fuzz);
         mp.born = p.born;
+        mp.draft = p.draft;
         mp.ratings = p.ratings;
         contract = genContract(mp);
 
